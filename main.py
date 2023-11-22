cd151527055dda0984ea0b0a742a9053228546e9
mefgner <mefgner@gmail.com>
2023-11-22 18:32:51 +0200

    Added config file Added ability to work with cmd args

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import json
 import time
 import psutil
 from pynput import keyboard
 from ResChanger import reschanger
+# import argparse
 
 Keys = keyboard.Key
 last_btn: Keys
@@ -53,8 +55,24 @@ def auto60hz(time_step=1, w=1920, h=1080, perf_rate=144, eff_rate=60, startup_sw
     
         
 if __name__ == '__main__':
-    l = keyboard.Listener(on_press=on_press, on_release=on_release)
-    l.start()
-    auto60hz()
-    l.stop()
+    # arg = argparse.ArgumentParser(
+    #     prog="Auto60HZ",
+    #     description="Auto switches refresh rate if power state switched",
+    # )
     
+    # arg.add_argument("-wx", "--width", type=int, help="Resolution width")
+    # arg.add_argument("-hx", "--height", type=int, help="Resolution height")
+    # arg.add_argument("-pr", "--performance-rate", type=int, help="Resolution refresh rate(charger plugged-in)")
+    # arg.add_argument("-er", "--efficiency-rate", type=int, help="Resolution refresh rate(charger plugged-out)")
+    
+    # arg.add_argument("--do-startup-switch", action="store_true", help="Program comparing current state of charger with current refresh rate and switches it.")
+    # params = dict(arg.parse_args())
+    
+    with open('config.json') as f:
+        stream = f.read()
+        params = json.JSONDecoder().decode(stream)
+    
+        with keyboard.Listener(on_press=on_press, on_release=on_release) as l:
+            auto60hz(w=params['width'], h=params['height'], 
+                    perf_rate=params['performance_refresh_rate'], 
+                    eff_rate=params['efficiency_refresh_rate'])
