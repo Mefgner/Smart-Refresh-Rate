a777e178ca9b079fbdeb82d8d0689b58d90e7589
mefgner <mefgner@gmail.com>
2023-12-01 23:12:56 +0200

    Changes Bugfixes: improved stability Logs: writing logs if program catch exception States: now program changes also resolution on power state switch

--- installer.bat
+++ installer.bat
@@ -1,6 +1,6 @@
-python -m venv venv
+@echo off
 .\venv\Scripts\pip3.exe install psutil pynput pyinstaller
-.\venv\Scripts\pyinstaller.exe -n Auto60HZ -D --add-data "config.json:." --clean main.py
+.\venv\Scripts\pyinstaller.exe -n Auto60HZ --noconsole -F --add-data "config.json:." --clean main.py
 mkdir %USERPROFILE%\AppData\Local\Auto60HZ
 copy dist\Auto60HZ.exe %USERPROFILE%\AppData\Local\Auto60HZ\Auto60HZ.exe /y
-REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Auto60HZ /t REG_SZ /d %USERPROFILE%\AppData\Local\Auto60HZ\Auto60HZ.exe
+REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Auto60HZ /t REG_SZ /d %USERPROFILE%\AppData\Local\Auto60HZ\Auto60HZ.exe /f
--- main.py
+++ main.py
@@ -34,6 +34,7 @@ def on_press(key):
 def on_release(key):
     if key == Keys.delete and last_btn == Keys.shift_r:
         reschanger.set_display_defaults()
+        write_logs(Exception("Emergency exit from the program"))
         exit()
 
 
@@ -48,8 +49,6 @@ async def change_screen_settings(ss: ScreenSettings):
         write_logs(e)
         await asyncio.sleep(5)
         reschanger.set_resolution(ss.width, ss.height, ss.refresh_rate)
-    finally:
-        exit()
 
 
 async def switch_rate(current_state, prss: ScreenSettings, psss: ScreenSettings):
@@ -78,26 +77,25 @@ async def auto60hz(time_step: int, prss: ScreenSettings, psss: ScreenSettings, s
 
 
 async def main():
-
     with open('config.json') as config:
         stream = config.read()
         params = json.JSONDecoder().decode(stream)
 
-        with keyboard.Listener(on_press=on_press, on_release=on_release) as keyb:
-            powersave_dict = params["powersave-state"]
-            powersave_state = ScreenSettings(
-                width=powersave_dict["width"],
-                height=powersave_dict["height"],
-                refresh_rate=powersave_dict["refresh-rate"]
-            )
+        powersave_dict = params["powersave-state"]
+        powersave_state = ScreenSettings(
+            width=powersave_dict["width"],
+            height=powersave_dict["height"],
+            refresh_rate=powersave_dict["refresh-rate"]
+        )
 
-            performance_dict = params["performance-state"]
-            performance_state = ScreenSettings(
-                width=performance_dict["width"],
-                height=performance_dict["height"],
-                refresh_rate=performance_dict["refresh-rate"]
-            )
+        performance_dict = params["performance-state"]
+        performance_state = ScreenSettings(
+            width=performance_dict["width"],
+            height=performance_dict["height"],
+            refresh_rate=performance_dict["refresh-rate"]
+        )
 
+        with keyboard.Listener(on_press=on_press, on_release=on_release):
             await auto60hz(TIME_STEP, performance_state, powersave_state, STARTUP_SWITCH)
 
 
