27b90ba74d4b0101a7d605f912df1c104afd9418
mefgner <mefgner@gmail.com>
2024-05-08 15:40:58 +0300

    fix config file isn't visible for installed program

--- installer.bat
+++ installer.bat
@@ -1,7 +1,8 @@
 @echo off
 python -m venv venv
 .\venv\Scripts\pip3.exe install psutil pynput pyinstaller
-.\venv\Scripts\pyinstaller.exe -n Auto60HZ --noconsole -F --add-data "config.json:." --clean main.py
+.\venv\Scripts\pyinstaller.exe -n Auto60HZ --noconsole -F --clean main.py
 mkdir %USERPROFILE%\AppData\Local\Auto60HZ
 copy dist\Auto60HZ.exe %USERPROFILE%\AppData\Local\Auto60HZ\Auto60HZ.exe /y
+copy config.json %USERPROFILE%\AppData\Local\Auto60HZ\config.json /y
 REG ADD HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run /v Auto60HZ /t REG_SZ /d %USERPROFILE%\AppData\Local\Auto60HZ\Auto60HZ.exe /f
