5cfcfcb428a3b58a97e4d0503d6e6dd680d3bf64
Mefgner <mefgner@gmail.com>
2024-07-04 22:07:27 +0300

    Made builders more smart

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
 @echo off
-python -m venv .venv
-.\.venv\Scripts\pip3.exe install -r requirements.txt
+if not exist .venv (
+    python -m venv .venv
+    .\.venv\Scripts\pip3.exe install -r requirements.txt
+)
 .\.venv\Scripts\pyinstaller.exe --uac-admin --noconsole -n SRR -F --clean main.py
--- test.bat
+++ test.bat
@@ -1,6 +1,8 @@
 @echo off
 rmdir /s /q %localappdata%\SRR
-python -m venv .venv
-.\.venv\Scripts\pip3.exe install -r requirements.txt
+if not exist .venv (
+    python -m venv .venv
+    .\.venv\Scripts\pip3.exe install -r requirements.txt
+)
 .\.venv\Scripts\pyinstaller.exe -n SRR-test -F --clean main.py
 .\dist\SRR-test.exe
