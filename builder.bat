bc4fff1af27214f199ba2d9d9207965a573d57ef
Mefgner <mefgner@gmail.com>
2024-07-04 20:31:35 +0300

    Update builder.bat
    Added user admin prompt

--- builder.bat
+++ builder.bat
@@ -1,4 +1,4 @@
 @echo off
 python -m venv .venv
 .\.venv\Scripts\pip3.exe install -r requirements.txt
-.\.venv\Scripts\pyinstaller.exe --noconsole -n SRR -F --clean main.py
+.\.venv\Scripts\pyinstaller.exe --uac-admin --noconsole -n SRR -F --clean main.py
