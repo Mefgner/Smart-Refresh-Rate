c5520647fc4353a4ff00baefa3214531803a687d
Mefgner <mefgner@gmail.com>
2024-07-04 22:13:12 +0300

    Fixed builders

--- build.bat
+++ build.bat
@@ -2,5 +2,6 @@
 if not exist .venv (
     python -m venv .venv
     .\.venv\Scripts\pip3.exe install -r requirements.txt
+    .\.venv\Scripts\pip3.exe install pyinstaller
 )
 .\.venv\Scripts\pyinstaller.exe --uac-admin --noconsole -n SRR -F --clean main.py
--- test.bat
+++ test.bat
@@ -3,6 +3,7 @@ rmdir /s /q %localappdata%\SRR
 if not exist .venv (
     python -m venv .venv
     .\.venv\Scripts\pip3.exe install -r requirements.txt
+    .\.venv\Scripts\pip3.exe install pyinstaller
 )
 .\.venv\Scripts\pyinstaller.exe -n SRR-test -F --clean main.py
 .\dist\SRR-test.exe
